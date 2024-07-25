6831983bd1fe56889b0b96a991a2de407d88290c
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-07-25 13:15:48 +0530

    initial commit

---
+++ run.py
@@ -0,0 +1,23 @@
+import subprocess
+import threading
+
+
+# change these paths according to your setup
+ZOO_KEEPER_START_FILE_COMMAND = 'E:/kafka/bin/windows/zookeeper-server-start.bat E:/kafka/config/zookeeper.properties'
+KAFKA_SERVER_START_FILE_COMMAND = 'E:/kafka/bin/windows/kafka-server-start.bat E:/kafka/config/server.properties'
+
+def start_process(cmd, shell = True, start_new_thread = True):
+    try:
+        subprocess.run(cmd , shell=shell, start_new_session=start_new_thread)
+    except:
+        raise Exception
+
+def main():
+    p1 = threading.Thread(target=start_process, args= (ZOO_KEEPER_START_FILE_COMMAND,))
+    p2 = threading.Thread(target=start_process, args= (KAFKA_SERVER_START_FILE_COMMAND,))
+
+    p1.start()
+    p2.start()
+
+if __name__ == '__main__':
+    main()
