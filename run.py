fe2bffeb44cbb9efc83f06b9ecc9422f54b1a95b
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-07-27 16:48:37 +0530

    updating the run.py file to give the zookeeper server time to load before starting the kafka server.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import subprocess
 import threading
+import time
+import sys
 
 
 # change these paths according to your setup
@@ -9,15 +11,25 @@ KAFKA_SERVER_START_FILE_COMMAND = 'E:/kafka/bin/windows/kafka-server-start.bat E
 def start_process(cmd, shell = True, start_new_thread = True):
     try:
         subprocess.run(cmd , shell=shell, start_new_session=start_new_thread)
+        
     except:
         raise Exception
 
 def main():
+    print('running...')
     p1 = threading.Thread(target=start_process, args= (ZOO_KEEPER_START_FILE_COMMAND,))
-    p2 = threading.Thread(target=start_process, args= (KAFKA_SERVER_START_FILE_COMMAND,))
-
     p1.start()
+
+    time.sleep(20)
+
+    p2 = threading.Thread(target=start_process, args= (KAFKA_SERVER_START_FILE_COMMAND,))
     p2.start()
 
+
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    
+    
